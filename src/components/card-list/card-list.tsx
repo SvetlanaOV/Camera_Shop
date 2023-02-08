9aa28148e7dfa99e2168ae4c986a2cf2e51019ce
Svetlana Ovsiannikova <springtime070@mail.ru>
2023-02-08 15:31:26 +0200

    Добавляет компоненты

---
+++ src/components/card-list/card-list.tsx
@@ -0,0 +1,22 @@
+import {Fragment} from 'react';
+import Card from '../card/card';
+import { Camera } from '../../types/camera';
+
+type CardListProps = {
+  cameras: Camera[];
+}
+
+function CardList ({cameras}: CardListProps): JSX.Element {
+  return (
+    <Fragment>
+      {cameras.map((camera) => (
+        <Card
+          key={camera.id}
+          camera={camera}
+        />
+      ))}
+    </Fragment>
+  );
+}
+
+export default CardList;
---
+++ src/pages/main-page/main-page.tsx
@@ -0,0 +1,23 @@
+import Banner from '../../components/banner/banner';
+import Breadcrumbs from '../../components/breadcrumbs/breadcrumbs';
+import Catalog from '../../components/catalog/catalog';
+import Header from '../../components/header/header';
+import Footer from '../../components/footer/footer';
+
+function MainPage(): JSX.Element {
+  return (
+    <div className="wrapper">
+      <Header />
+      <main>
+        <Banner />
+        <div className="page-content">
+          <Breadcrumbs />
+          <Catalog />
+        </div >
+      </main >
+      <Footer />
+    </div>
+  );
+}
+
+export default MainPage;
