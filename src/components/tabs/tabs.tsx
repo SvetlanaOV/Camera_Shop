94817e70b13b739984d979cc8a507c6982aa5636
Svetlana Ovsiannikova <springtime070@mail.ru>
2023-02-15 12:48:34 +0200

    Добавляет реализацию табов

--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -8,6 +8,8 @@ function Tabs(): JSX.Element {
 
   const [activeTab, setActiveTab] = useState(TabName.Features);
 
+  const {vendorCode, category, type, level} = currentCamera;
+
   return (
     <div className="tabs product__tabs">
       <div className="tabs__controls product__tabs-controls">
@@ -18,16 +20,16 @@ function Tabs(): JSX.Element {
         <div className={`tabs__element ${activeTab === TabName.Features ? 'is-active' : ''}`}>
           <ul className="product__tabs-list">
             <li className="item-list"><span className="item-list__title">Артикул:</span>
-              <p className="item-list__text">{currentCamera.vendorCode}</p>
+              <p className="item-list__text">{vendorCode}</p>
             </li>
             <li className="item-list"><span className="item-list__title">Категория:</span>
-              <p className="item-list__text">{currentCamera.category}</p>
+              <p className="item-list__text">{category}</p>
             </li>
             <li className="item-list"><span className="item-list__title">Тип камеры:</span>
-              <p className="item-list__text">{currentCamera.type}</p>
+              <p className="item-list__text">{type}</p>
             </li>
             <li className="item-list"><span className="item-list__title">Уровень:</span>
-              <p className="item-list__text">{currentCamera.level}</p>
+              <p className="item-list__text">{level}</p>
             </li>
           </ul>
         </div>
