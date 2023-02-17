c3025b51b0b1e94e97e6df1c3431ccc141856f12
Svetlana Ovsiannikova <springtime070@mail.ru>
2023-02-17 16:44:09 +0200

    Добавляет мелкие правки и компонент модалки для отпраки отзыва

--- src/components/review-list/review-list.tsx
+++ src/components/review-list/review-list.tsx
@@ -32,6 +32,11 @@ function ReviewList(): JSX.Element {
           </button>
         </div>
       </div>
+      <a className="up-btn" href="#header">
+        <svg width="12" height="18" aria-hidden="true">
+          <use xlinkHref="#icon-arrow2"></use>
+        </svg>
+      </a>
     </section>
   );
 }
